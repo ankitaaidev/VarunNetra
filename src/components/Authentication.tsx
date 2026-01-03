4f19e13f8023ddcaf908df6c9d6094ef5d7e6ac1
Ankita Roy <contact.ankitaroy@gmail.com>
2026-01-04 01:50:27 +0530

    Update Authentication.tsx

--- src/components/Authentication.tsx
+++ src/components/Authentication.tsx
@@ -5,7 +5,7 @@ import { Input } from './ui/input';
 import { Label } from './ui/label';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from './ui/tabs';
 import { Eye, EyeOff, Phone, Mail } from 'lucide-react';
-import varunNetraLogo from 'figma:asset/e44aa1704c39379f905619749b1bc1e6c107d354.png';
+import varunNetraLogo from 'src:asset/logo.png';
 
 interface AuthenticationProps {
   language: string;
